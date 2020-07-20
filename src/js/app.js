3dd3d71f26a2daa923c00696baf7551da008dee8
Tolu Lawson <lawsontolu1@gmail.com>
2020-07-20 17:23:54 +0100

    -- Add features section

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,9 @@ require('bootstrap');
 $(() => {
   $('.menu-button').click(() => {
     if (!$('#menu').is(':checked')) {
-      $('.menu-item').addClass('animate__animated animate__fadeInRight');
+      $('.menu-item').addClass('animate__animated animate__fadeInRight animate__fast');
     } else {
-      $('.menu-item').removeClass('animate__animated animate__fadeInRight');
+      $('.menu-item').removeClass('animate__animated animate__fadeInRight animate__fast');
     }
   });
 });
