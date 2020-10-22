d01091a5b460a3533de3219dc84c4ee074a88ede
Tolu Lawson <lawsontolu1@gmail.com>
2020-10-22 21:05:36 +0100

    -- Update Azure API key

--- src/js/app.js
+++ src/js/app.js
@@ -9,13 +9,8 @@ const humanizeDuration = require('humanize-duration');
 const xmlbuilder = require('xmlbuilder');
 require('datejs');
 require('jquery-contextmenu');
-const firebase = require('firebase/app');
-require('firebase/firestore');
-// require('jquery.ui.position');
 const isURL = require('is-url');
-
-let azureKey = '';
-let aylienKey = '';
+const { azureKey, aylienKey } = require('./api_keys');
 
 const getWordCountFromScript = (script) => {
   const regex = /\b(\w+)\b/g;
@@ -94,31 +89,6 @@ $(() => {
         playButtonView.playButton.click();
       }
     }
-  };
-
-  // const firebaseAPI = 'AIzaSyA4Q_VjmoThK_gIdecNb6iZAZ1EvcwRoow';
-
-  const initializeFirebase = () => {
-    firebase.initializeApp({
-      apiKey: firebaseAPI,
-      authDomain: 'apis-51900.firebaseapp.com',
-      projectId: 'apis-51900',
-    });
-
-    const db = firebase.firestore();
-
-    db.collection('api-keys').doc('api-keys-document')
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          azureKey = doc.data().azure;
-          aylienKey = doc.data().aylien;
-          playFromURLParam();
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   };
 
   const textAreaView = {
@@ -592,7 +562,6 @@ $(() => {
       playWidgetView.init();
       wpmValueView.init();
       menuView.init();
-      initializeFirebase();
       this.injectFooter();
     },
 
